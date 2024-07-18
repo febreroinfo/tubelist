89054fb3919d27aa4ec36f5a5477a96326d1f3ef
JFebrero <febrero.info@gmail.com>
2024-07-17 22:54:12 -0400

    Deleted File

--- assets/scripts/service.js
+++
@@ -1,9 +0,0 @@
-// Check the browser to make sure it supports service workers
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
-    // Register the path to the service worker file
-    .register('./serviceWorker.js')
-    .then(function() {
-        console.log('Service Worker is Registered');
-    })
-}
